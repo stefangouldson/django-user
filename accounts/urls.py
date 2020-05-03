2600f6109ce0aec603be56fd0b7ea91c4a9768b8
Stefan Gouldson <stefangouldson@gmail.com>
2020-05-03 20:06:05 +0100

    register page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('treasure/', views.treasure, name='treasure'),
+    path('register/', views.register, name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -6,3 +7,13 @@ def home(request):
 
 def treasure(request):
     return render(request, 'treasure.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+        return render(request,'registration.html', {'form':form})
