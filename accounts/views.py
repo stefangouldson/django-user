db0a6d833488af26322c849009fe0736e9ec0ee3
Stefan Gouldson <stefangouldson@gmail.com>
2020-05-04 18:46:59 +0100

    form errors shown

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -17,7 +18,8 @@ def register(request):
             form.save()
             return redirect('login')
         else:
-            print('error')
+            messages.error(request, "Error")
+            return render(request,'registration/registration.html', {'form':form})
     else:
         form = UserCreationForm()
         return render(request,'registration/registration.html', {'form':form})
